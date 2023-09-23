ddf5fdc18440af85dd126a0806f59f44d814fca7
Bruno Membrado <brunomembrado10@gmail.com>
2023-09-23 16:39:52 -0300

    hash homework fixed

--- HashFunctions&HashTables/index.js
+++ HashFunctions&HashTables/index.js
@@ -157,6 +157,330 @@ class HashTableClass {
         }
     }
 }
+/*
+HashTable with Map
+Advantages:
+Dynamic Sizing: Maps in JavaScript (and many other languages) can dynamically resize. This can lead to better memory utilization.
+Built-in Methods: Maps come with built-in methods that make certain operations more straightforward, such as delete, has, and iterators like keys, values, and entries.
+No Explicit Collision Handling: The underlying implementation of a Map handles collisions, so you don't have to write code for that.
+Key Flexibility: Maps allow for more flexible key types. While your hash table implementation uses strings, Maps can use objects, functions, and other types as keys.
+Disadvantages:
+Performance Overhead: As seen in tests, Maps can introduce some performance overheads, possibly due to their dynamic nature and added functionalities.
+Complexity: While Maps handle many complexities for you, understanding their underlying workings might be more challenging than a simple array.
+Memory Usage: Due to their dynamic nature and added functionalities, Maps might consume more memory compared to an array, especially for very small datasets.
+
+*/
+class HashTableClassArray {
+    constructor(size = 100) {
+        this.size = size;
+        this.table = new Array(size).fill(null);
+    }
+    customHashFunction(key) {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            hash += key.charCodeAt(i);
+        }
+        return hash % this.size;
+    }
+    set(key, value) {
+        const index = this.customHashFunction(key);
+        if (this.table[index] === null) {
+            this.table[index] = [{ key, value }];
+        }
+        else {
+            const existingEntry = this.table[index].find(entry => entry.key === key);
+            if (existingEntry) {
+                existingEntry.value = value;
+            }
+            else {
+                this.table[index].push({ key, value });
+            }
+        }
+    }
+    get(key) {
+        const index = this.customHashFunction(key);
+        if (this.table[index] === null) {
+            throw new Error(`The key ${key} doesn't exist in the HASH TABLE`);
+        }
+        else {
+            const entry = this.table[index].find(e => e.key === key);
+            if (entry) {
+                return entry.value;
+            }
+            else {
+                throw new Error(`The key ${key} doesn't exist in the HASH TABLE`);
+            }
+        }
+    }
+    delete(key) {
+        const index = this.customHashFunction(key);
+        if (this.table[index] === null) {
+            throw new Error(`The key ${key} doesn't exist in the HASH TABLE`);
+        }
+        else {
+            this.table[index] = this.table[index].filter(entry => entry.key !== key);
+            if (this.table[index].length === 0) {
+                this.table[index] = null;
+            }
+        }
+    }
+}
+/*
+HashTable with Array
+Advantages:
+Predictable Memory Usage: Arrays have a fixed size (when they're not dynamically resizing), so they can be more memory efficient for smaller datasets.
+Direct Access: Arrays allow direct access to the memory slot, making them slightly faster in certain operations.
+Simplicity: Arrays provide a simpler data structure for straightforward hashing scenarios without the need for many of the bells and whistles that come with maps.
+Performance: The array-based hash table performed better in tests. This can be attributed to the overhead introduced by map-based operations and internal mechanisms.
+Disadvantages:
+Fixed Size: Traditional arrays are of fixed size. This means you must either allocate a large chunk of memory upfront or implement dynamic resizing, which can be complex.
+Handling Collisions: Arrays require explicit handling of hash collisions, which can add complexity to the code.
+Memory Wastage: If the hash table size is much larger than the number of entries, there's potential for wasted memory.
+Manual Management: Array-based implementations often require more manual management of data storage and retrieval.
+*/
+/*
+The choice between an array-based hash table and a map-based one largely depends on the requirements of the project.
+ If absolute control over every aspect of the hashing process is essential, and you're willing to invest the
+ time to handle collisions, resizing, and other intricacies manually for the sake of performance or memory efficiency,
+ an array-based hash table would be more suitable. This is often the case in performance-intensive applications or
+ environments with limited resources.
+
+On the other hand, if the primary concerns are flexibility, ease of development, and handling a dynamically changing dataset,
+then a map-based hash table is the way to go. It's a powerful tool for many modern software applications where development speed, adaptability,
+and handling complex data types are more pressing than raw performance.
+
+Remember, while these guidelines provide a general direction, always profile and test in your specific environment and use case to make an informed decision.
+*/
+/*
+Should the hash set have a has() method?
+
+Yes, it should. However it wasn’t in the exercise so I avoided it. The hash method is important to know the existence of a value in the HashTable returning a boolean true if exists and false in the counterpart.
+
+What will happen if you exceed 100 items, and what should happen in this case?
+
+If the HashTable has a size of one hundred and it is exceeded in more than one hundred entries, as described in the comments, the methods of the table will tend to a time complexity of O(m) where m is the quantity of values that the array linked to the key hashed has.
+However O(m) is not the time complexity that we are looking for, therefore we can take measures such as resizing the HashTable, which means creating a new and bigger one, re-locating all the entries of the previous table into the new and huge one.
+
+*/
+class HashTableClassMapResizeHas {
+    constructor(size = 3) {
+        // Using a small size (3) for testing purposes
+        this.size = size;
+        this.table = new Map();
+        this.entries = 0;
+        this.loadFactor = 0.75;
+    }
+    /**
+     * Resize Method
+     * This method resizes the hash table when the number of keys stored reaches a certain threshold (like a load factor).
+     * We double the size of the table, and re-hash all the current keys into this new table.
+     *
+     * Time complexity: O(n) where n is the number of keys in the table, as each key needs to be reinserted.
+     */
+    resize() {
+        if (this.entries > this.size * this.loadFactor) {
+            // resizing when load factor exceeds 0.75
+            const oldTable = new Map(this.table);
+            this.size *= 2;
+            this.table = new Map();
+            this.entries = 0; // resetting entries to 0 because we'll increment it while reinserting
+            oldTable.forEach((value, key) => {
+                value.forEach(entry => {
+                    this.set(entry.key, entry.value);
+                });
+            });
+        }
+    }
+    /**
+     * Hash Function
+     * The customHashFunction takes a string and computes a hash code by summing the char codes of all characters in the string.
+     * The hash code is then obtained as the remainder of the division of the sum by the size of the hash table, which helps in evenly distributing the keys across the hash table to a certain extent.
+     *
+     * Time complexity: O(n) where n is the length of the key string.
+     *
+     * @param {string} key - The key to be hashed
+     * @returns {string} - The computed hash code as a string
+     */
+    customHashFunction(key) {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            hash += key.charCodeAt(i);
+        }
+        return (hash % this.size).toString();
+    }
+    /**
+     * Set Method
+     * The set method takes a key and a value, computes the hash of the key using the customHashFunction, and stores the key-value pair in the hash table.
+     * In the event of a collision (where another key hashes to the same index), the key-value pair is added to a list at that index.
+     * If the key already exists, its value is updated.
+     *
+     * Time complexity:
+     * - Best and Average case: O(1)
+     * - Worst case: O(m) where m is the number of collisions in the worst-case scenario
+     *
+     * @param {string} key - The key to be added to the hash table
+     * @param {any} value - The value to be associated with the key
+     */
+    set(key, value) {
+        const index = this.customHashFunction(key);
+        let tableEntry = this.table.get(index);
+        if (tableEntry !== undefined) {
+            let existingEntry = tableEntry.find(entry => entry.key === key);
+            if (existingEntry) {
+                existingEntry.value = value;
+            }
+            else {
+                tableEntry.push({ key, value });
+                this.entries++; // incrementing the entries because a new key is added
+            }
+        }
+        else {
+            this.table.set(index, [{ key, value }]);
+            this.entries++; // incrementing the entries because a new key is added
+        }
+        this.resize(); // check if resizing is required
+    }
+    /**
+     * Get Method
+     * This method computes the hash of the provided key and looks up the value associated with the key in the hash table.
+     * If the key does not exist, it throws an error.
+     *
+     * Time complexity:
+     * - Best and Average case: O(1)
+     * - Worst case: O(m) where m is the number of collisions at a specific index
+     *
+     * @param {string} key - The key whose associated value is to be retrieved
+     * @returns {any} - The value associated with the key
+     */
+    get(key) {
+        const index = this.customHashFunction(key);
+        let tableEntry = this.table.get(index);
+        if (tableEntry !== undefined) {
+            const hashTableEntry = tableEntry.find(entry => entry.key === key);
+            if (hashTableEntry !== undefined) {
+                return hashTableEntry.value;
+            }
+            else {
+                throw new Error(`The key ${key} doesn't exist in the HASH TABLE`);
+            }
+        }
+        else {
+            throw new Error(`The key ${key} doesn't exist in the HASH TABLE`);
+        }
+    }
+    has(key) {
+        try {
+            this.get(key);
+            return true;
+        }
+        catch (error) {
+            return false;
+        }
+    }
+    /**
+     * Delete Method
+     * This method computes the hash for the given key and removes the key-value pair from the hash table.
+     * If the key does not exist in the table, it throws an error.
+     *
+     * Time complexity:
+     * - Best and Average case: O(1)
+     * - Worst case: O(m) where m is the number of collisions at a specific index
+     *
+     * @param {string} key - The key to be removed from the hash table
+     */
+    delete(key) {
+        const index = this.customHashFunction(key);
+        let tableEntry = this.table.get(index);
+        if (tableEntry !== undefined) {
+            this.table.set(index, tableEntry.filter(entry => entry.key !== key));
+            this.entries--;
+        }
+        else {
+            throw new Error(`The key ${key} doesn't exist in the HASH TABLE`);
+        }
+    }
+}
+class HashTableClassArrayResizeHas {
+    constructor(size = 3) {
+        this.size = size;
+        this.table = new Array(size).fill(null);
+        this.entries = 0;
+        this.loadFactor = 0.75;
+    }
+    customHashFunction(key) {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            hash += key.charCodeAt(i);
+        }
+        return hash % this.size;
+    }
+    resize() {
+        if (this.entries <= this.size * this.loadFactor)
+            return; // if under the load factor, return early
+        const oldTable = [...this.table];
+        this.size *= 2;
+        this.table = new Array(this.size).fill(null); // fix here
+        this.entries = 0; // reset count
+        oldTable.forEach(array => {
+            array && array.forEach(entry => this.set(entry.key, entry.value));
+        });
+    }
+    has(key) {
+        try {
+            this.get(key);
+            return true;
+        }
+        catch (error) {
+            return false;
+        }
+    }
+    set(key, value) {
+        const index = this.customHashFunction(key);
+        if (!this.table[index]) {
+            this.table[index] = [{ key, value }];
+            this.entries++;
+        }
+        else {
+            const existingEntry = this.table[index].find(entry => entry.key === key);
+            if (existingEntry) {
+                existingEntry.value = value;
+            }
+            else {
+                this.table[index].push({ key, value });
+                this.entries++;
+            }
+        }
+        this.resize();
+    }
+    get(key) {
+        const index = this.customHashFunction(key);
+        if (!this.table[index]) {
+            throw new Error(`The key ${key} doesn't exist in the HASH TABLE`);
+        }
+        const entry = this.table[index].find(e => e.key === key);
+        if (entry) {
+            return entry.value;
+        }
+        else {
+            throw new Error(`The key ${key} doesn't exist in the HASH TABLE`);
+        }
+    }
+    delete(key) {
+        const index = this.customHashFunction(key);
+        if (!this.table[index]) {
+            throw new Error(`The key ${key} doesn't exist in the HASH TABLE`);
+        }
+        const initialLength = this.table[index].length;
+        this.table[index] = this.table[index].filter(entry => entry.key !== key);
+        this.entries -= initialLength - this.table[index].length; // decrement by the number of entries removed
+        if (this.table[index].length === 0) {
+            this.table[index] = null;
+        }
+    }
+}
 module.exports = {
-    HashTableClass
+    HashTableClass,
+    HashTableClassArray,
+    HashTableClassMapResizeHas,
+    HashTableClassArrayResizeHas
 };
--- HashFunctions&HashTables/index.test.js
+++ HashFunctions&HashTables/index.test.js
@@ -1,4 +1,9 @@
-const { HashTableClass } = require('./index.js');
+const {
+  HashTableClass,
+  HashTableClassArray,
+  HashTableClassMapResizeHas,
+  HashTableClassArrayResizeHas
+} = require('./index.js');
 
 describe('HashTableClass', () => {
   test('should set and get values correctly, including handling collisions', () => {
@@ -51,5 +56,182 @@ describe('HashTableClass', () => {
     // Other values, including those that collided with the deleted key, should still be retrievable
     expect(hashTable.get('name')).toBe('Alice');
     expect(hashTable.get('ba')).toBe('value2');
+  });
+});
+
+describe('HashTable Performance Test array vs map', () => {
+  test('Map-based HashTableClass performance', () => {
+    const table = new HashTableClass(1000);
+    const start = Date.now();
+
+    for (let i = 0; i < 10000; i++) {
+      table.set(`key${i}`, `value${i}`);
+    }
+
+    for (let i = 0; i < 10000; i++) {
+      table.get(`key${i}`);
+    }
+
+    const duration = Date.now() - start;
+    console.log(`Map-based HashTableClass took ${duration}ms`);
+  });
+
+  test('Array-based HashTableClassArray performance', () => {
+    const table = new HashTableClassArray(1000);
+    const start = Date.now();
+
+    for (let i = 0; i < 10000; i++) {
+      table.set(`key${i}`, `value${i}`);
+    }
+
+    for (let i = 0; i < 10000; i++) {
+      table.get(`key${i}`);
+    }
+
+    const duration = Date.now() - start;
+    console.log(`Array-based HashTableClassArray took ${duration}ms`);
+  });
+});
+
+/*
+Map-based HashTableClass took 129ms
+Array-based HashTableClassArray took 89ms
+*/
+
+describe('HashTableClass functionality', () => {
+  it('should resize the table correctly', () => {
+    const hashTable = new HashTableClassMapResizeHas(3);
+    hashTable.set('key1', 'value1');
+    hashTable.set('key2', 'value2');
+
+    expect(hashTable.size).toBe(3); // It should still be 3
+
+    hashTable.set('key3', 'value3'); // This should trigger resizing
+    expect(hashTable.size).toBe(6); // New size should be double of 3 = 6
+    expect(hashTable.get('key1')).toBe('value1');
+    expect(hashTable.get('key2')).toBe('value2');
+    expect(hashTable.get('key3')).toBe('value3');
+  });
+
+  it('should correctly identify if a key exists', () => {
+    const hashTable = new HashTableClassMapResizeHas(3);
+    hashTable.set('key1', 'value1');
+
+    expect(hashTable.has('key1')).toBe(true); // Since 'key1' was added, it should exist
+    expect(hashTable.has('key2')).toBe(false); // 'key2' was never added, so it shouldn't exist
+  });
+
+  it('should handle the existence of keys regardless of their values', () => {
+    const hashTable = new HashTableClassMapResizeHas(3);
+    hashTable.set('key1', false); // Even though the value is a falsy value, the key exists
+    hashTable.set('key2', 0); // Another falsy value
+
+    expect(hashTable.has('key1')).toBe(true);
+    expect(hashTable.has('key2')).toBe(true);
+  });
+});
+
+describe('HashTableClassMapResizeHas', () => {
+  let hashTable;
+
+  beforeEach(() => {
+    hashTable = new HashTableClassMapResizeHas();
+  });
+
+  it('should set and get a key-value pair', () => {
+    hashTable.set('testKey', 'testValue');
+    expect(hashTable.get('testKey')).toBe('testValue');
+  });
+
+  it('should handle collisions', () => {
+    hashTable.set('a', 'valueA');
+    hashTable.set('d', 'valueD'); // assuming the hash function causes a collision for keys 'a' and 'd'
+    expect(hashTable.get('a')).toBe('valueA');
+    expect(hashTable.get('d')).toBe('valueD');
+  });
+
+  it('should resize when exceeding load factor', () => {
+    const spy = jest.spyOn(hashTable, 'resize');
+    for (let i = 0; i < 4; i++) {
+      hashTable.set(`key${i}`, `value${i}`);
+    }
+    expect(spy).toHaveBeenCalled();
+    spy.mockRestore();
+  });
+
+  it('should indicate if a key exists using has', () => {
+    hashTable.set('existingKey', 'value');
+    expect(hashTable.has('existingKey')).toBe(true);
+    expect(hashTable.has('nonExistingKey')).toBe(false);
+  });
+});
+
+describe('HashTableClassArrayResizeHas', () => {
+  let hashTable;
+
+  beforeEach(() => {
+    hashTable = new HashTableClassArrayResizeHas();
+  });
+
+  it('should set and get a key-value pair', () => {
+    hashTable.set('testKey', 'testValue');
+    expect(hashTable.get('testKey')).toBe('testValue');
+  });
+
+  it('should handle collisions', () => {
+    hashTable.set('a', 'valueA');
+    hashTable.set('d', 'valueD'); // assuming the hash function causes a collision for keys 'a' and 'd'
+    expect(hashTable.get('a')).toBe('valueA');
+    expect(hashTable.get('d')).toBe('valueD');
+  });
+
+  it('should resize when exceeding load factor', () => {
+    for (let i = 0; i < 4; i++) {
+      hashTable.set(`key${i}`, `value${i}`);
+    }
+    expect(hashTable.size).toBeGreaterThan(3);
+  });
+
+  it('should indicate if a key exists using has', () => {
+    hashTable.set('existingKey', 'value');
+    expect(hashTable.has('existingKey')).toBe(true);
+    expect(hashTable.has('nonExistingKey')).toBe(false);
+  });
+});
+
+describe('Performance Comparison', () => {
+  const NUM_ENTRIES = 10000; // adjust as needed
+  const keys = [...Array(NUM_ENTRIES).keys()].map(String);
+
+  it('performance of HashTableClassMapResizeHas', () => {
+    const hashTable = new HashTableClassMapResizeHas();
+    const start = jest.getRealSystemTime();
+
+    keys.forEach((key, index) => {
+      hashTable.set(key, `value${index}`);
+    });
+
+    keys.forEach(key => {
+      hashTable.get(key);
+    });
+
+    const end = jest.getRealSystemTime();
+    console.log(`HashTableClassMapResizeHas took ${end - start}ms`);
+  });
+
+  it('performance of HashTableClassArrayResizeHas', () => {
+    const hashTable = new HashTableClassArrayResizeHas();
+    const start = jest.getRealSystemTime();
+
+    keys.forEach((key, index) => {
+      hashTable.set(key, `value${index}`);
+    });
+
+    keys.forEach(key => {
+      hashTable.get(key);
+    });
+
+    const end = jest.getRealSystemTime();
+    console.log(`HashTableClassArrayResizeHas took ${end - start}ms`);
   });
 });

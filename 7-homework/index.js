82dedfe781f11403e995ecbda191e4cddd091856
Bruno Membrado <brunomembrado10@gmail.com>
2024-03-12 11:38:34 -0300

    7-hom-2-5 fixed

--- 7-homework/index.js
+++ 7-homework/index.js
@@ -69,47 +69,136 @@ function testSortAlgorithm(arr, sortAlgorithm) {
             return Date.now() - time;
     }
 }
-function generateArr(length) {
+function generateArr(length, type) {
     let arr = [];
     for (let index = 0; index < length; index++) {
         let randomNumber = Math.round(Math.random() * 1000);
         arr.push(randomNumber);
     }
-    return arr;
+    switch (type) {
+        case 'sorted':
+            return arr.sort((a, b) => a - b);
+        case 'reverse':
+            return arr.sort((a, b) => b - a);
+        default:
+            return arr;
+    }
 }
-for (let index = 0; index < 50; index++) {
-    let arr = generateArr(index * index * 3);
-    let quickTime = testSortAlgorithm(arr, 'quick');
-    let bubbleTime = testSortAlgorithm(arr, 'bubble');
-    let mergeTime = testSortAlgorithm(arr, 'merge');
-    results.push({
-        arrLength: arr.length,
-        quickSortTime: quickTime,
-        bubbleSortTime: bubbleTime,
-        mergeSortTime: mergeTime,
-        faster: quickTime < bubbleTime && quickTime < mergeTime
-            ? 'quick'
-            : bubbleTime < quickTime && bubbleTime < mergeTime
+let consecutiveTimes = { method: '-', times: 0, arrLength: 0 };
+for (let index = 0; index < 40; index++) {
+    let arrSorted = generateArr(index * index * 3, 'sorted');
+    let arrReverse = generateArr(index * index * 3, 'reverse');
+    let arrRandom = generateArr(index * index * 3, 'random');
+    let quickTimeSorted = testSortAlgorithm(arrSorted, 'quick');
+    let quickTimeReverse = testSortAlgorithm(arrReverse, 'quick');
+    let quickTimeRandom = testSortAlgorithm(arrRandom, 'quick');
+    let bubbleTimeSorted = testSortAlgorithm(arrSorted, 'bubble');
+    let bubbleTimeReverse = testSortAlgorithm(arrReverse, 'bubble');
+    let bubbleTimeRandom = testSortAlgorithm(arrRandom, 'bubble');
+    let mergeTimeSorted = testSortAlgorithm(arrSorted, 'merge');
+    let mergeTimeReverse = testSortAlgorithm(arrReverse, 'merge');
+    let mergeTimeRandom = testSortAlgorithm(arrRandom, 'merge');
+    let sortFastest = quickTimeSorted < bubbleTimeSorted && quickTimeSorted < mergeTimeSorted
+        ? 'quick'
+        : bubbleTimeSorted < quickTimeSorted && bubbleTimeSorted < mergeTimeSorted
+            ? 'bubble'
+            : mergeTimeSorted < quickTimeSorted && mergeTimeSorted < bubbleTimeSorted
+                ? 'merge'
+                : 'none';
+    let reverseFastest = quickTimeReverse < bubbleTimeReverse && quickTimeReverse < mergeTimeReverse
+        ? 'quick'
+        : bubbleTimeReverse < quickTimeReverse &&
+            bubbleTimeReverse < mergeTimeReverse
+            ? 'bubble'
+            : mergeTimeReverse < quickTimeReverse &&
+                mergeTimeReverse < bubbleTimeReverse
+                ? 'merge'
+                : 'none';
+    let randomFastest = quickTimeRandom < bubbleTimeRandom && quickTimeRandom < mergeTimeRandom
+        ? 'quick'
+        : bubbleTimeRandom < quickTimeRandom && bubbleTimeRandom < mergeTimeRandom
+            ? 'bubble'
+            : mergeTimeRandom < quickTimeRandom && mergeTimeRandom < bubbleTimeRandom
+                ? 'merge'
+                : 'none';
+    let winner = sortFastest === 'quick' &&
+        reverseFastest === 'quick' &&
+        randomFastest === 'quick'
+        ? 'quick'
+        : sortFastest === 'merge' &&
+            reverseFastest === 'merge' &&
+            randomFastest === 'merge'
+            ? 'merge'
+            : sortFastest === 'bubble' &&
+                reverseFastest === 'bubble' &&
+                randomFastest === 'bubble'
                 ? 'bubble'
-                : mergeTime < quickTime && mergeTime < bubbleTime
-                    ? 'merge'
-                    : '-'
+                : '-';
+    consecutiveTimes = {
+        times: consecutiveTimes.method === winner ? consecutiveTimes.times + 1 : 1,
+        method: winner,
+        arrLength: consecutiveTimes.method === winner
+            ? consecutiveTimes.arrLength
+            : arrSorted.length
+    };
+    results.push({
+        arrLength: arrSorted.length,
+        quickSortTime: {
+            sortArr: quickTimeSorted,
+            reverseArr: quickTimeReverse,
+            randomArr: quickTimeRandom
+        },
+        bubbleSortTime: {
+            sortArr: bubbleTimeSorted,
+            reverseArr: bubbleTimeReverse,
+            randomArr: bubbleTimeRandom
+        },
+        mergeSortTime: {
+            sortArr: mergeTimeSorted,
+            reverseArr: mergeTimeReverse,
+            randomArr: mergeTimeRandom
+        },
+        faster: winner
     });
+    if (consecutiveTimes.method !== '-' && consecutiveTimes.times >= 3) {
+        break;
+    }
 }
+console.log("Ra: Random array | Re: Reverse array | S: Sorted array");
 console.log('Results for Sorting Algorithm Performance Analysis\n');
-console.log('Array Length | QuickSort Time (ms) | BubbleSort Time (ms) | MergeSort Time (ms) | Faster');
+console.log('Array Length | QuickSort Time (ms)     | BubbleSort Time (ms)      | MergeSort Time (ms)        | Faster');
 console.log('-'.repeat(90));
 for (const result of results) {
-    console.log(`${result.arrLength.toString().padEnd(13)}|` +
-        `${result.quickSortTime.toString().padEnd(22)}|` +
-        `${result.bubbleSortTime.toString().padEnd(22)}|` +
-        `${result.mergeSortTime.toString().padEnd(19)}|` +
-        `${result.faster.padEnd(8)}`);
+    console.log(`${result.arrLength.toString().padEnd(13)} ` +
+        `${('Ra: ' +
+            result.quickSortTime.randomArr.toString() +
+            ' | S: ' +
+            result.quickSortTime.sortArr.toString() +
+            ' | Re: ' +
+            result.quickSortTime.reverseArr.toString() +
+            ' |').padEnd(26)}` +
+        `${('Ra: ' +
+            result.bubbleSortTime.randomArr.toString() +
+            ' | S: ' +
+            result.bubbleSortTime.sortArr.toString() +
+            ' | Re: ' +
+            result.bubbleSortTime.reverseArr.toString() +
+            ' |').padEnd(28)}` +
+        `${('Ra: ' +
+            result.mergeSortTime.randomArr.toString() +
+            ' | S: ' +
+            result.mergeSortTime.sortArr.toString() +
+            ' | Re: ' +
+            result.mergeSortTime.reverseArr.toString() +
+            ' |').padEnd(28)}` +
+        `${result.faster}`);
 }
 console.log('-'.repeat(90));
+console.log(`${results[results.length - 3].faster} started to win consitently at:  ${results[results.length - 3].arrLength} array length`);
 console.log(`
 Time complexity of Bubble Sort:
 Due to the fact that this algorithm has a nested loop, it is always O(n^2) where n is the length of the array to sort.
+
 It is the slowest sort algorithm because of it exponencial time duration related with array items.
 `);
 console.log(`
